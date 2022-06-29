1e142ada48701000807b9c793c71f37b4cdf8fd1
moham <momaher74>
2022-06-29 18:08:28 +0200

    window size

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:hh/cubit/app_cubit.dart';
 import 'package:hh/helpers/shared/dio.dart';
 import 'package:hh/helpers/shared/sharedpref.dart';
 import 'package:hh/layout/screens/homescreen.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'cubit/observar.dart';
 
@@ -11,6 +12,17 @@ void main()async {
   WidgetsFlutterBinding.ensureInitialized();
   DioHelper.init();
   await CacheHelper.init();
+  windowManager.waitUntilReadyToShow().then((_) async {
+    await windowManager.setTitleBarStyle(TitleBarStyle.hidden);
+    await windowManager.setBackgroundColor(Colors.transparent);
+    await windowManager.setSize(const Size(800, 600));
+    await windowManager.setMinimumSize(const Size(800, 600));
+    await windowManager.setMaximumSize(const Size(double.infinity, double.infinity));
+    await windowManager.center();
+    await windowManager.show();
+    await windowManager.focus();
+    await windowManager.setSkipTaskbar(false);
+  });
   BlocOverrides.runZoned(
         () => runApp(const MyApp()),
     blocObserver: AppBlocObserver(),
@@ -23,7 +35,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  BlocProvider<AppCubit>(
-      create: (context)=>AppCubit()..createDB()..name(),
+      create: (context)=>AppCubit()..createDB(),
       child: BlocConsumer<AppCubit,AppState>(
         listener: (context , state){},
         builder: (context , state ){
